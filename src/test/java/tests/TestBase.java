67bfd813ae85aeffe5d7bb9c77f52f7e654b10e7
Anya_Hello_World <domnichanya@gmail.com>
2022-06-22 21:07:29 +0300

    add commit with allure changes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,16 +1,28 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PractiseFormPage;
 
 public class TestBase {
     PractiseFormPage practiseFormPage = new PractiseFormPage();
 
     @BeforeAll
-    static void beforeAll(){
+    static void setUp(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wb/hub";
     }
 
 }
