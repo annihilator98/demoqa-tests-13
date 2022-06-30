37140eb192e401a0e3755ab481af55256f587dbb
Anya_Hello_World <domnichanya@gmail.com>
2022-06-30 07:58:15 +0300

    Merge remote-tracking branch 'origin/pageobjects' into pageobjects
    # Conflicts:
    #	src/test/java/tests/TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,7 +28,7 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wb/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 
